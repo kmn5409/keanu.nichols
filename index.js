7fbdd6ae8c94b99c3e617416a91326b2bcefd6f5
kmn5409 <nichols.keanu9@gmail.com>
2017-10-30 23:20:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var one = "#10e88a",
 		four = "#f88f18",
 		five = "#bb6774";
 
-(jQuery)(window).on("scroll touchmove", function() {
-		if ((document).scrollTop() >= ("#one").position().top) {
-				('body').css('background', ("#one").attr("data-color"));
+$(window).on("scroll touchmove", function() {
+		if ($(document).scrollTop() >= $("#one").position().top) {
+				$('body').css('background', $("#one").attr("data-color"));
 
 		};
 		if ($(document).scrollTop() > $("#two").position().top) {
