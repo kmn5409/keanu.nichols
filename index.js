f4b8c89729c63c1b76b5be6c5a8dda16d0f38bf8
kmn5409 <nichols.keanu9@gmail.com>
2017-10-30 22:11:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ var one = "#10e88a",
 		five = "#bb6774";
 
 (window).on("scroll touchmove", function() {
-		if ($(document).scrollTop() >= $("#one").position().top) {
-				$('body').css('background', $("#one").attr("data-color"));
+		if ((document).scrollTop() >= ("#one").position().top) {
+				('body').css('background', ("#one").attr("data-color"));
 
 		};
-		if ($(document).scrollTop() > $("#two").position().top) {
-				$('body').css('background', $("#two").attr("data-color"))
+		if ((document).scrollTop() > ("#two").position().top) {
+				('body').css('background', ("#two").attr("data-color"))
 		};
-		if ($(document).scrollTop() > $("#three").position().top) {
+		if ((document).scrollTop() > ("#three").position().top) {
 
-				$('body').css('background', $("#three").attr("data-color"))
+				('body').css('background', ("#three").attr("data-color"))
 		};
-		if ($(document).scrollTop() > $("#four").position().top) {
+		if ((document).scrollTop() > ("#four").position().top) {
 
-				$('body').css('background', $("#four").attr("data-color"))
+				('body').css('background', ("#four").attr("data-color"))
 		};
-		if ($(document).scrollTop() >= $("#five").position().top) {
-				$('body').css('background', $("#five").attr("data-color"))
+		if ((document).scrollTop() >= ("#five").position().top) {
+				('body').css('background', ("#five").attr("data-color"))
 		};
 });
